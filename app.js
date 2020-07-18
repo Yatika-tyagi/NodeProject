44d092aff90500b87cfa1c898729168799b5526e
yatika tyagi <yatika.tyagi@kelltontech.com>
2020-07-18 14:14:48 +0530

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 var express = require('express');
 var app = express();
 var routes_v1 = require('./src/routes/v1/routes_v1')
  
 app.get('/', function (req, res){
    res.send({
-      message: "Sorry, we are unable to fetch data"
+      name: 'Yatika Tyagi',
+      designation: 'Senior Software engineer'
    });
 });
 
 app.use('/api/v1', routes_v1);
-app.use(bodyParser);
+app.use(express.json());
 var port = 3001;
 app.listen(port);
 console.log('App is runing on port' + ' ' + port);
--- src/controllers/atmDenomination.js
+++ src/controllers/atmDenomination.js
@@ -1,30 +1,22 @@
-
 exports.dispenseCase = function(req, res) {
-  const inputAmount = req.query.inputAmount;
-  const accountBalance = req.query.accountBalance;
-  var outputArray = [];
- 
-  if(inputAmount !== 0 && inputAmount % 100 == 0 && inputAmount <= accountBalance) {
-    var noteOf2000 = Math.round(inputAmount / 2000);
-    var noteOf500 = Math.round((inputAmount - (noteOf2000 * 2000)) / 500 );
-    // var noteOf200 = Math.round((inputAmount - ((noteOf2000 * 2000) + (noteOf500 * 500))) / 200)
-    var noteOf100 = Math.round((inputAmount - ((noteOf2000 * 2000) + (noteOf500 * 500))) / 100);
+  const withdrawAmount = req.query.withdrawAmount;
+   if(withdrawAmount <= 0 || withdrawAmount%100 !== 0){
+      res.send(' please enter correct amount ');
+    } else {
+        res.send(minNumberOfNote(withdrawAmount))
+  } 
+  };
 
-    outputArray.push(noteOf2000);
-    outputArray.push(noteOf500);
-    // outputArray.push(noteOf200);
-    outputArray.push(noteOf100);
+var minNumberOfNote = function(withdrawAmount){
+  var totalNotesCount = {}
+  let ATMAmountTypes = [2000,500,200,100];
+  for(let amountType of ATMAmountTypes) {
+      noteCount = Math.floor(withdrawAmount/amountType);
+      if(noteCount > 0) {
+          totalNotesCount[amountType] = noteCount || 1;
+          withdrawAmount = withdrawAmount% amountType;
 
-   console.log("Balance in your account left = ", accountBalance - inputAmount);
-   console.log(outputArray);
-   }
-   else if (inputAmount > accountBalance) {
-   
-   console.log("Insufficient amount in your account");
-   }
-   else if ( inputAmount % 100 != 0 || inputAmount < 100 ) {
-   console.log( "Invalid Input, Amount should be multiples of 100");
- }
-  res.send(outputArray);   
-  
-};
+      }
+  }
+   return totalNotesCount;
+}
